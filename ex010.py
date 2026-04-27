9cf95e8cd449c244221b51aa62d264ebdd602c23
PeduRibeiro <opeduu@gmail.com>
2026-04-27 14:30:45 -0300

    Update ex010.py

--- ex010.py
+++ ex010.py
@@ -1,5 +1,3 @@
 reais= float(input('Quannto dinheiro em reais você gostaria de converter? R$ '))
-
 dolar= reais/3.27
-
 print('Esse valor equivale a {} dolares, e lhe sobrara US${:.2f}'.format(reais, dolar))
