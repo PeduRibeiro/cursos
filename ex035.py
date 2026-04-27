0b2b0cd7bb948c6cb0247c8b1261eaf8bca05e9b
PeduRibeiro <opeduu@gmail.com>
2026-04-27 14:33:45 -0300

    Refactor input and triangle validation logic

--- ex035.py
+++ ex035.py
@@ -1,7 +1,6 @@
 A = int(input('Digite a maior reta: '))
 B = int(input('Digite o valor da reta 2: '))
 C = int(input('Digite o valor da reta 3: '))
-
 if (C + B) <= A:
     print('Sendo {} {} {} os valores das retas NÃO É possivel formar um triangulo'.format(A, B, C))
 else:
