008133c31894614727b69bcaf76e1f6f9bf5bbed
PeduRibeiro <opeduu@gmail.com>
2026-04-27 14:31:53 -0300

    Refactor code to remove unnecessary line deletions

--- ex017.py
+++ ex017.py
@@ -1,6 +1,5 @@
 import math
 cata = float(input('Digite o cateto adjacente: '))
 cato = float(input('Digite o cateto oposto:' ))
-
 hipo = math.hypot(cata, cato)
 print('Sendo {}, e {} os catetos do seu triangulo a hipotenusa sera igual a {},'.format(cata, cato, hipo))
