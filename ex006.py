cad12eb53963d095557d3ab1723d8a5d71415cf3
PeduRibeiro <opeduu@gmail.com>
2026-04-27 14:26:38 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -1,3 +1,2 @@
 a = int(input('Digite um valor: '))
-
 print('Sendo seu numero: {} \n \nO doblo dele será: {} \nO triplo dele será: {} \nA sua raiz quadrada será: {}'.format(a, a*2, a*3, pow(a, (1/2))))
