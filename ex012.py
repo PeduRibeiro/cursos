75bd740b33853fd6f0edc11c8ed1e39acee3e7bc
PeduRibeiro <opeduu@gmail.com>
2026-04-27 14:31:02 -0300

    Remove unnecessary line breaks in ex012.py

--- ex012.py
+++ ex012.py
@@ -1,6 +1,4 @@
 preco1 = float(input('Qual o valor do produto? R$ '))
-
 preco2 = preco1 * 0.95
-
 print('Sabendo que o valor do produto é R$ {} com o desconto de 5% será pago R$ {}'.format(preco1, preco2))
 
