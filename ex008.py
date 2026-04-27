7cff7b502fb379047814249f1981be23a2db3517
PeduRibeiro <opeduu@gmail.com>
2026-04-27 14:30:21 -0300

    Remove unnecessary line in ex008.py

--- ex008.py
+++ ex008.py
@@ -1,3 +1,2 @@
 m= float(input('Qual o valor em metros?'))
-
 print('Sabendo que o valor em metros é: {}m  \nO valor em centimetros será: {}cm \nE em milimetros será: {}mm'.format(m, m*100, m*1000))
