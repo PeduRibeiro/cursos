096f418eac660d490bc3f079136be2f2378231e8
PeduRibeiro <opeduu@gmail.com>
2026-04-27 14:25:53 -0300

    Fix input prompts and print statement for sum

--- ex003.py
+++ ex003.py
@@ -1,12 +1,4 @@
-r'''dia=input('Dia?')
-mes= input('Mês?')
-ano= input('Ano?')
-print('Você nasceu no dia', dia, 'de', mes, 'de', ano,' Correto?')'''
-
 a=int(input('Qual o primeiro numero?'))
 b=int(input('Qual o segundo numero?'))
-
 soma = a + b
-#print('A soma entre',a,'e',b,'vale:', soma)
-
 print('A soma entre {} e {} vale {}'.format(a, b, soma))
