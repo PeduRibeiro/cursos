105d7c767c8dda68e8816d547a70bf3677d24029
PeduRibeiro <opeduu@gmail.com>
2026-04-27 14:29:15 -0300

    Refactor name input and check for 'silva'

--- ex025.py
+++ ex025.py
@@ -1,7 +1,6 @@
-nome = str(input('Digite um nome completo: '))
+nome = str(input('Digite um nome completo: ')).lower() .strip()
 nome.find('silva')
 cond=nome.find('silva')
-
 if cond == -1:
     print('NÃO')
 else:
