57e206f9d229be4519c598f58a4ebc4b592d7ca5
PeduRibeiro <opeduu@gmail.com>
2026-04-27 14:32:04 -0300

    Remove unnecessary blank lines in ex018.py

--- ex018.py
+++ ex018.py
@@ -1,8 +1,6 @@
 import math
 angulo = float(input('Digite um angulo: '))
-
 sin = math.sin(math.radians(angulo))
 cos = math.cos(math.radians(angulo))
 tan = math.tan(math.radians(angulo))
-
 print('Sendo {}° o seu angulo, temos como seno {:.2f}°, cosseno{:.2f}°, tangente{:.2f}°,' .format(angulo, sin, cos, tan))
